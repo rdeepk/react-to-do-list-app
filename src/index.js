5f558afc2342cb9b37f829e5332cd88da6f98388
Ramandeep Kaur <rdeepk@gmail.com>
2017-10-25 15:33:55 -0700

    merged with starter code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const status = [
+    {
+        id: 100,
+        title: 'Active'
+    },
+    {
+        id: 101,
+        title: 'Complete'
+    },
+    {
+        id: 102,
+        title: 'Not Started'
+    }
+]
+
+const tasks = {
+
+}
+
+const projects = [
+
+]
+
+const lables = [
+
+]
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
     
